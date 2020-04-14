e721ec0dc5dd94914178bf314ed4167c2182e230
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-04-14 19:34:49 +0200

    Search works, but case sensitive and only for title

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -12,4 +12,16 @@ router.get("/", function (req, res, next) {
     })
 })
 
+
+router.get("/search", function (req, res, next) {
+    let q = req.query.q;
+    console.log("getting suggestions for: ", q)
+    UserPlant
+    .find({"title" : {$regex : `${q}.*`}})
+    .then((plants) => {
+        console.log("all filtered plants:", plants)
+        res.json(plants)
+    })
+})
+
 module.exports = router;
